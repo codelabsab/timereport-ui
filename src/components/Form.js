444db611c005a24bd57d308db21f05c610e6dd19
Mattias Lundberg <mattias.lundberg@codelabs.se>
2020-10-23 13:42:25 +0200

    Fix loading data on page load

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,7 @@ class Form extends Component {
                 style={{maxWidth: '50%', float:'left'}}
                 value={this.props.selectedOption}
                 onChange={this.props.handleSelectChange}>
-                <option disabled hidden>Select a Username</option>
+                <option hidden>Select a Username</option>
                 {Object.entries(this.props.names).map(name => (
                     <option key={shortid.generate()} value={name}>
                         {name[1]}
